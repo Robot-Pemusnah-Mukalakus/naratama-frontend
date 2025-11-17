053978b2bfaeec38b7d4a7e7f92cca01528981c8
SentientCorn <radityanarotama0@gmail.com>
2025-11-17 20:48:14 +0700

    feat: add Content Security Policy headers to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,27 @@ const nextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' https://app.sandbox.midtrans.com https://snap-assets.al-pc-id-b.cdn.gtfblabs.io;
+              frame-src https://app.sandbox.midtrans.com https://app.midtrans.com;
+              connect-src 'self' https://app.sandbox.midtrans.com;
+              img-src 'self' data: https:;
+              style-src 'self' 'unsafe-inline';
+            `.replace(/\s+/g, " "), // remove newlines
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
